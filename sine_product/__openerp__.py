242fdfaf88b0a89a3d795b31ef0abef2cbac8457
dhecar <dhecar@gmail.com>
2016-09-27 17:42:27 +0200

    first commit

---
+++ sine_product/__openerp__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{'name': 'Product adaptations for All2Grow',
+ 'version': '1.0.0',
+ 'category': 'Custom',
+ 'depends': ['product'],
+ 'author': 'Sinergiainformatica.net. David Hernández',
+ 'license': 'AGPL-3',
+ 'description': """ Add new fields in product model""",
+ 'data': ['views/product_view.xml'],
+ 'installable': True,
+ 'application': True,
+}
